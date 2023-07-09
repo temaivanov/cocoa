b845f6069bf78dea05842c4c3564b485b7b06c75
artem <artyom-ivanov-spb@yandex.ru>
2023-07-09 21:27:24 +0300

    structuring the pages with jinja

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/signup')
 def signup():
-    return '<p>signup form</p>'
+    return render_template("signup.html")
 
 @auth.route('/login')
 def login():
-    return '<p>login form</p>'
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
