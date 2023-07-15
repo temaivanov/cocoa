495620de3c26ead6e7e5acd1922da43ecf6d6e35
artem <artyom-ivanov-spb@yandex.ru>
2023-07-15 22:02:39 +0300

    adding the processing of http requests

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
+#here we get do define what methods each endpoint can process. By default, they all can only process GET.
 @auth.route('/signup')
 def signup():
     return render_template("signup.html")
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
