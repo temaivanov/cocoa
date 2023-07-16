427b0516ca5872d743720e42a2accc61d7d580d2
artem <artyom-ivanov-spb@yandex.ru>
2023-07-16 22:08:25 +0300

    intruoducing flask's flashed messages

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,29 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 #here we get do define what methods each endpoint can process. By default, they all can only process GET.
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        #here I introduced the flash messages from flask. Here =flash= is just an intention, and this conent will not go eve into the console.
+        #to render the text of a notifiaction, describe the template and conditions of a notification in the base HTML
+        if len(email) < 7:
+            flash('Email address must contain at least 7 characters', category = 'error')
+        elif len(firstName) < 1:
+            flash('The firstname should be at least one character', category = 'error')
+        elif len(password1) < 5:
+            flash('Password length should be at least 5 characters', category = 'error')    
+        elif password1 != password2:
+            flash('Passwords do not match' , category = 'error')
+        else: 
+            flash('Success!', category = 'success')
+    
     return render_template("signup.html")
 
 @auth.route('/login', methods=['GET', 'POST'])
