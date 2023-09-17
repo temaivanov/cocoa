ed10dd103e4e9478eb238e64bd6ad408010399f2
artem <artyom-ivanov-spb@yandex.ru>
2023-09-17 19:37:26 +0300

    database instance created

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,19 @@
 #making the 'website' folder a python package by adding the __init__.py into the directory
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-#create a new database
+#create == innitiate ==  a new database
 
 db = SQLAlchemy()
-DB_NAME = "artemdb.db"
+DB_NAME = "artem2.db"
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///aretmdb.db'
+    #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///artem1.db"
+
     db.init_app(app)
 
     from .auth import auth
@@ -20,5 +23,13 @@ def create_app():
     app.register_blueprint(auth, url_prefix = '/auth')
     app.register_blueprint(views, url_prefix = '/')
 
+    from .models import User, Note
+    create_database(app)
 
     return app
+
+def create_database(app):
+    if not os.path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print("DB CREATED")
--- website/models.py
+++ website/models.py
@@ -1,18 +1,22 @@
-#this database model will be using the data structured that I created to store notes
+#this database model will be using the data structure that I created to store notes
 #this is how all notes and users should look like
-from website import db
+#e.g. all of my notes and user will have to conform to the rules below
+
+#import from the website folder, from the __init__ file
+from . import db 
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    data = db.Column(db.String(100000))
-    date = db.Column(db.DateTime(timezone = True), default =func.now())
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     #how do we associate a note to a user? we would need a connection between notes and users
-    user_id = db.Coulmn(db.Integer, db.ForeignKey('user.id')) #user is an object of User class
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #user is an object of User class
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(150), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note') #when we do a relathionship in SQLAlchemy, we reference the name of the class
